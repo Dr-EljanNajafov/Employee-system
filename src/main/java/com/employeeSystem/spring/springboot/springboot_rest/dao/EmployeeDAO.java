f31361205191755d2f2a76c964fe5e2da223a0f2
Eljan Najafov <eljan.najafov02@mail.ru>
2024-08-23 17:34:42 +0400

    added JPARepository

--- src/main/java/com/employeeSystem/spring/springboot/springboot_rest/dao/EmployeeDAO.java
+++ src/main/java/com/employeeSystem/spring/springboot/springboot_rest/dao/EmployeeDAO.java
@@ -1,16 +1,7 @@
 package com.employeeSystem.spring.springboot.springboot_rest.dao;
 
 import com.employeeSystem.spring.springboot.springboot_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface EmployeeDAO {
-    public List<Employee> getAllEmployees();
-
-    public void saveEmployee(Employee employee);
-
-    public Employee getEmployee(int id);
-
-    public void deleteEmployee(int id);
-
+public interface EmployeeDAO extends JpaRepository<Employee, Integer> {
 }
